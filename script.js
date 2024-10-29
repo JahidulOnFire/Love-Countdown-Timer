2a41ad8c533424d493131bfba98f99f8e33154e1
Jahidul Hassan Parvez <115993964+JahidulOnFire@users.noreply.github.com>
2024-10-29 12:30:58 +0600

    Date change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const countdownDate = new Date("November 15, 2024 00:00:00").getTime();
+const countdownDate = new Date("January 30, 2025 00:00:00").getTime();
 
 const updateCountdown = () => {
     const now = new Date().getTime();
